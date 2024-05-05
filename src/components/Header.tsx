6c8e2f759c9db54c13ee237f45237e881722bf47
dzl-i <denzel.iskandar@gmail.com>
2024-05-06 01:30:45 +0700

    fix: spacing issues

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,11 @@ import { TypingAnimation } from "./TypingAnimation"
 
 export const Header = () => {
   return (
-    <section id="about" className="flex flex-row items-start gap-5">
+    <section id="header" className="flex flex-row items-start gap-5 sm:h-60 h-44">
       <StarsCanvas />
       <div className='flex flex-col justify-center items-center mt-5'>
         <div className='w-5 h-5 rounded-full bg-primary' />
-        <div className='w-1 sm:h-60 h-40 bg-gradient-to-b from-primary' />
+        <div className='w-1 sm:h-60 h-44 bg-gradient-to-b from-primary' />
       </div>
 
       <div>
