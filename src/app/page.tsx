8fc893e510942db4233aa31374cd1c589016e400
dzl-i <denzel.iskandar@gmail.com>
2024-05-10 00:27:40 +0700

    feat: add button for contact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import { Header } from "@/components/Header";
 import { StarsCanvas } from "@/components/Stars";
 import { Timeline } from "@/components/Timeline";
 
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="w-full">
@@ -13,6 +16,13 @@ export default function Home() {
       <div className="flex flex-col max-w-7xl mx-auto sm:px-16 px-6 sm:gap-y-24 gap-y-20 mb-12">
         <About />
         <Timeline />
+      </div>
+      <div className="flex justify-center items-center mb-12">
+        <Link href={"/contact"}>
+          <Button color="primary" variant="shadow">
+            Contact Me
+          </Button>
+        </Link>
       </div>
     </main>
   );
