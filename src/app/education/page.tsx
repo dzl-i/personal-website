a9d333608cfb3fe27b65c467840133ae8d8f4c5d
dzl-i <denzel.iskandar@gmail.com>
2024-05-13 17:17:51 +0700

    fix: deleted education page

--- src/app/education/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Education() {
-  return (
-    <div className="sm:min-h-[calc(100vh-241px)] min-h-[calc(100vh-285px)] flex items-center justify-center">
-      <p>Work in progress, come back soon!</p>
-    </div>
-  )
-}
